adec4cbe666b7ccd2696f4fc7fb0a201ea33c9f2
TJBaumgartner <tnorth6@gmail.com>
2023-07-14 10:54:20 -0700

    restart on project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import { useState } from 'react';
 import './App.css';
-import PersonalInfo from './components/personal';
-import './App.css'
-import WorkExperience from './components/experience';
-import Education from './components/education';
+import React, { useState } from 'react';
+import uniqid from "uniqid";
 
 function App() {
   return (
-    <div className="App">
-      <PersonalInfo/>
-      <WorkExperience/>
-      <Education/>
+    <div>
+
     </div>
   );
 }
--- src/components/personal.js
+++ src/components/personal.js
@@ -2,22 +2,23 @@ const PersonalInfo = () => {
     return (
         <div className="PersonalInfoForm">
             <label htmlFor="nameInput">
-                <input type="text" id="nameInput" placeholder="John Human"></input>
+                <input type="text" id="nameInput" placeholder="Name" style={{width: "100%"}}></input>
             </label>
             <label htmlFor="titleInput">
-                <input type="text" id="titleInput" placeholder="title"></input>
+                <input type="text" id="titleInput" placeholder="Title" style={{width: "100%"}}></input>
             </label>
             <label htmlFor="numberInput">
-                <input type="text" id="numberInput" placeholder="123456789"></input>
+                <input type="text" id="numberInput" placeholder="Phone" style={{width: "100%"}}></input>
             </label>
             <label htmlFor="emailInput">
-                <input type="text" id="emailInput" placeholder="email@email.com"></input>
+                <input type="text" id="emailInput" placeholder="Email" style={{width: "100%"}}></input>
             </label>
             <label htmlFor="locationInput">
-                <input type="text" id="locationInput" placeholder="germany"></input>
+                <input type="text" id="locationInput" placeholder="Country" style={{width: "100%"}}></input>
             </label>
             <label htmlFor="descriptionInput">
-                <input type="text" id="descriptionInput" placeholder="some text..."></input>
+                <textarea type="text" id="descriptionInput" placeholder="Description..." style={{height: "100px",
+            width: "100%", resize: "none"}}></textarea>
             </label>
         </div>
     )
